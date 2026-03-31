d445b1e3c9284e90d84b383f42bd6407e617c4db
IronLB <mrkpd2001@gmail.com>
2026-03-31 06:46:48 +0300

    Add project files.

---
+++ AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ReactiveUI;
+using System.Reactive;
+
+namespace AvaloniaApplication2.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        private string _name = "";
+        private string _message = "";
+
+        public string Name
+        {
+            get => _name;
+            set 
+            {
+                this.RaiseAndSetIfChanged(ref _name, value);
+                this.RaisePropertyChanged(nameof(Greeting));
+            } 
+        }
+
+        public string Message
+        {
+            get => _message;
+            set => this.RaiseAndSetIfChanged(ref _message, value);
+        }
+
+        public string Greeting => string.IsNullOrEmpty(_name)
+            ? "Name unknown..."
+            : $"Hello, {_name}!";
+
+        public ReactiveCommand<Unit, Unit> SayHelloCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            var canExecute = this.WhenAnyValue(
+                x => x.Name,
+                name => !string.IsNullOrWhiteSpace(name));
+
+            SayHelloCommand = ReactiveCommand.Create(() =>
+            {
+                Message = $"{Name}!?";
+            }, canExecute);
+        }
+    }
+}
---
+++ AvaloniaApplication2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
+
+namespace AvaloniaApplication2.ViewModels
+{
+    public abstract class ViewModelBase : ReactiveObject
+    {
+    }
+}
