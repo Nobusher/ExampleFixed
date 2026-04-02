cbdf158a73d5e35988bd064c773c18983588af3f
Nobusher <TheGreatestMind1337@yandex.ru>
2026-04-02 18:43:54 +0300

    Добавлен file manager

--- AvaloniaApplication2/ViewModels/FilesViewModel.cs
+++ AvaloniaApplication2/ViewModels/FilesViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +12,31 @@ namespace AvaloniaApplication2.ViewModels
     internal class FilesViewModel : ViewModelBase
     {
         public string Title => "File manager";
+
+        private string _filePath = "";
+        private string _fileContent = "";
+
+        public string FilePath 
+        {
+            get => _filePath;
+            set=>this.RaiseAndSetIfChanged(ref _filePath, value);
+        }
+        public string FileContent 
+        {
+            get => _fileContent;
+            set=>this.RaiseAndSetIfChanged(ref _fileContent, value);
+        }
+        public ReactiveCommand<Unit, Unit> OpenFileCommand { get; }
+
+        public FilesViewModel()
+        {
+            OpenFileCommand = ReactiveCommand.Create(() => { });
+        }
+        public void LoadFile(string filePath) 
+        {
+            FilePath = filePath;
+            FileContent = File.ReadAllText(filePath);
+        }
+
     }
 }
--- AvaloniaApplication2/Views/FilesView.axaml.cs
+++ AvaloniaApplication2/Views/FilesView.axaml.cs
@@ -1,7 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using System.Linq;
 namespace AvaloniaApplication2;
 
 public partial class FilesView : UserControl
@@ -9,5 +11,35 @@ public partial class FilesView : UserControl
     public FilesView()
     {
         InitializeComponent();
+    }
+    private async void OpenFileButton_Click(object sender,
+        RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel == null) { return; }
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync
+            (
+                new Avalonia.Platform.Storage.FilePickerOpenOptions
+                {
+                    Title = "Select a file",
+                    AllowMultiple = false,
+                    FileTypeFilter = new[]
+                    {
+                        new FilePickerFileType("Text files")
+                        {
+                            Patterns = new [] {"*.txt","*.cs",".json"}
+                        }
+                    }
+                }
+            );
+
+        if (files.Count == 0) { return; }
+
+        var path = files.First().Path.LocalPath;
+        if (DataContext is ViewModels.FilesViewModel vm) 
+        {
+            vm.LoadFile(path);
+        }
+
     }
 }
